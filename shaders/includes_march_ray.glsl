c7d1e64adb1368beca8da178ce7ef131796e565a
Ivo <ivotesligte@gmail.com>
2023-04-29 13:05:49 +0200

    changed logic

--- shaders/includes_march_ray.glsl
+++ shaders/includes_march_ray.glsl
@@ -2,25 +2,19 @@ bool marchRay(inout vec3 pos, vec3 dir, vec3 lmOrigin, float threshold, inout fl
     float dist = totalDist;
 
     int lmLayer = lmLayerAtPos(pos, lmOrigin);
-    float mult = (1.0 / float(LM_SIZE)) / LM_UNIT_SIZES[lmLayer];
-    vec3 idx = (pos - lmOrigin) * mult;
 
     for (uint i = 0; i < 100; i++) {
         pos += dir * dist;
 
-        idx += (dir * dist) * mult;
+        vec3 idx = pos - lmOrigin; // TODO: lmOrigin varying between layers
+        bool increaseLayer = any(greaterThan(abs(idx), vec3(float(LM_SIZE) * LM_UNIT_SIZES[lmLayer] * 0.5)));
+        bool decreaseLayer = lmLayer > 0 && all(lessThan(abs(idx), vec3(float(LM_SIZE) * LM_UNIT_SIZES[lmLayer] * 0.25)));
+        lmLayer += int(increaseLayer) - int(decreaseLayer); // one of: -1, 0, 1
 
-        if (any(greaterThan(abs(idx), vec3(0.5)))) {
-            idx *= 0.5;
-            mult *= 0.5;
-            lmLayer += 1;
-        } else if (lmLayer > 0 && all(lessThan(abs(idx), vec3(0.25)))) {
-            idx *= 2.0;
-            mult *= 2.0;
-            lmLayer -= 1;
-        }
+        float mult = (1.0 / float(LM_SIZE)) / LM_UNIT_SIZES[lmLayer]; // TODO: consts
+        vec3 idx2 = pos - lmOrigin; // TODO: lmOrigin varying between layers
 
-        dist = texture(SDFImages[lmLayer], clamp(idx + 0.5, 0.0, 1.0)).x;
+        dist = texture(SDFImages[lmLayer], clamp(idx2 * mult + 0.5, 0.0, 1.0)).x;
         totalDist += dist;
 
         if (lmLayer >= LM_COUNT) { // out of bounds
