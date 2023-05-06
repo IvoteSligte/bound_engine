483af9d1ee4a56cad0dd5fd743382b4e8436cf0e
Ivo <ivotesligte@gmail.com>
2023-05-06 12:03:29 +0200

    added custom image

--- src/images.rs
+++ src/images.rs
@@ -1,11 +1,10 @@
 use std::sync::Arc;
 
 use vulkano::{
-    device::Queue,
     format::Format,
     image::{
         view::{ImageView, ImageViewCreateInfo},
-        ImageCreateFlags, ImageDimensions, ImageUsage, ImageViewAbstract, StorageImage,
+        ImageCreateFlags, ImageDimensions, ImageUsage, ImageViewAbstract,
         SwapchainImage,
     },
     sampler::Sampler,
@@ -17,9 +16,11 @@ use crate::{
     shaders::{LM_COUNT, LM_RAYS, LM_SIZE},
 };
 
+use self::image::CustomImage;
+
 #[derive(Clone)]
 pub(crate) struct Images {
-    pub(crate) color: Arc<StorageImage>,
+    pub(crate) color: Arc<CustomImage>,
     pub(crate) lightmap: LightmapImages,
     pub(crate) swapchain: Vec<Arc<SwapchainImage>>,
     pub(crate) sampler: Arc<Sampler>,
@@ -54,15 +55,15 @@ impl Images {
 
 #[derive(Clone)]
 pub(crate) struct ImageViews {
-    pub(crate) color: Arc<ImageView<StorageImage>>,
+    pub(crate) color: Arc<ImageView<CustomImage>>,
     pub(crate) lightmap: LightmapImageViews,
 }
 
 pub(crate) fn create_color_image(
     allocators: Arc<Allocators>,
     window: Arc<Window>,
-) -> Arc<StorageImage> {
-    StorageImage::with_usage(
+) -> Arc<CustomImage> {
+    CustomImage::with_usage(
         &allocators.memory,
         ImageDimensions::Dim2d {
             width: window.inner_size().width,
@@ -72,17 +73,16 @@ pub(crate) fn create_color_image(
         Format::R16G16B16A16_UNORM, // double precision for copying to srgb
         ImageUsage::STORAGE | ImageUsage::TRANSFER_SRC,
         ImageCreateFlags::empty(),
-        [], // INFO: empty queue_family_indices sets SharingMode to Exclusive in vulkano 0.33.0 with no other side effects
     )
     .unwrap()
 }
 
 #[derive(Clone)]
 pub(crate) struct LightmapImages {
-    pub(crate) colors: Vec<Vec<Arc<StorageImage>>>,
-    pub(crate) staging_color: Arc<StorageImage>,
-    pub(crate) sdfs: Vec<Arc<StorageImage>>,
-    pub(crate) materials: Vec<Arc<StorageImage>>,
+    pub(crate) colors: Vec<Vec<Arc<CustomImage>>>,
+    pub(crate) staging_color: Arc<CustomImage>,
+    pub(crate) sdfs: Vec<Arc<CustomImage>>,
+    pub(crate) materials: Vec<Arc<CustomImage>>,
 }
 
 #[derive(Clone)]
@@ -104,13 +104,12 @@ impl LightmapImages {
 
         // TODO: layout optimisation for storage
         let create_storage_image = |usage, format| {
-            StorageImage::with_usage(
+            CustomImage::with_usage(
                 &allocators.memory,
                 dimensions,
                 format,
                 ImageUsage::STORAGE | usage,
                 ImageCreateFlags::empty(),
-                [], // INFO: empty queue_family_indices sets SharingMode to Exclusive in vulkano 0.33.0 with no other side effects
             )
             .unwrap()
         };
@@ -172,7 +171,7 @@ impl LightmapImages {
     }
 
     pub(crate) fn image_views(&self) -> LightmapImageViews {
-        let views = |imgs: &Vec<Arc<StorageImage>>, usage: ImageUsage| {
+        let views = |imgs: &Vec<Arc<CustomImage>>, usage: ImageUsage| {
             imgs.iter()
                 .map(|img| {
                     ImageView::new(
@@ -187,7 +186,11 @@ impl LightmapImages {
                 .collect()
         };
 
-        let colors_storage = self.colors.iter().map(|imgs| views(imgs, ImageUsage::STORAGE)).collect();
+        let colors_storage = self
+            .colors
+            .iter()
+            .map(|imgs| views(imgs, ImageUsage::STORAGE))
+            .collect();
         let sdfs_storage = views(&self.sdfs, ImageUsage::STORAGE);
         let materials_storage = views(&self.materials, ImageUsage::STORAGE);
 
@@ -200,6 +203,166 @@ impl LightmapImages {
             sdfs_storage,
             sdfs_sampled,
             materials_storage,
+        }
+    }
+}
+
+mod image {
+    use vulkano::{
+        device::{Device, DeviceOwned},
+        format::Format,
+        image::{
+            sys::{Image, ImageCreateInfo, RawImage},
+            traits::ImageContent,
+            ImageAccess, ImageCreateFlags, ImageDescriptorLayouts, ImageDimensions, ImageError,
+            ImageInner, ImageLayout, ImageUsage,
+        },
+        memory::{
+            allocator::{
+                AllocationCreateInfo, AllocationType, MemoryAllocatePreference, MemoryAllocator,
+                MemoryUsage,
+            },
+            DedicatedAllocation,
+        },
+        sync::Sharing,
+    };
+
+    use std::{
+        hash::{Hash, Hasher},
+        sync::{
+            atomic::{AtomicBool, Ordering},
+            Arc,
+        },
+    };
+
+    /// ADAPTED FROM vulkano::image::StorageImage
+    #[derive(Debug)]
+    pub(crate) struct CustomImage {
+        inner: Arc<Image>,
+
+        // If false, then the image is still `Undefined`.
+        layout_initialized: AtomicBool,
+    }
+
+    impl CustomImage {
+        /// Creates an image with a specific usage
+        pub(crate) fn with_usage(
+            allocator: &(impl MemoryAllocator + ?Sized),
+            dimensions: ImageDimensions,
+            format: Format,
+            usage: ImageUsage,
+            flags: ImageCreateFlags,
+        ) -> Result<Arc<CustomImage>, ImageError> {
+            assert!(!flags.intersects(ImageCreateFlags::DISJOINT)); // INFO: should be safe, but might not be
+
+            let raw_image = RawImage::new(
+                allocator.device().clone(),
+                ImageCreateInfo {
+                    flags,
+                    dimensions,
+                    format: Some(format),
+                    usage,
+                    sharing: Sharing::Exclusive, // TODO: Sharing::Concurrent implementation
+                    ..Default::default()
+                },
+            )?;
+            let requirements = raw_image.memory_requirements()[0];
+            let res = unsafe {
+                allocator.allocate_unchecked(
+                    requirements,
+                    AllocationType::NonLinear,
+                    AllocationCreateInfo {
+                        usage: MemoryUsage::DeviceOnly,
+                        allocate_preference: MemoryAllocatePreference::Unknown,
+                        ..AllocationCreateInfo::default()
+                    },
+                    Some(DedicatedAllocation::Image(&raw_image)),
+                )
+            };
+
+            match res {
+                Ok(alloc) => {
+                    let inner =
+                        Arc::new(raw_image.bind_memory([alloc]).map_err(|(err, _, _)| err)?);
+
+                    Ok(Arc::new(CustomImage {
+                        inner,
+                        layout_initialized: AtomicBool::new(false),
+                    }))
+                }
+                Err(err) => Err(err.into()),
+            }
+        }
+    }
+
+    unsafe impl DeviceOwned for CustomImage {
+        #[inline]
+        fn device(&self) -> &Arc<Device> {
+            self.inner.device()
+        }
+    }
+
+    unsafe impl ImageAccess for CustomImage {
+        #[inline]
+        fn inner(&self) -> ImageInner<'_> {
+            ImageInner {
+                image: &self.inner,
+                first_layer: 0,
+                num_layers: self.inner.dimensions().array_layers(),
+                first_mipmap_level: 0,
+                num_mipmap_levels: 1,
+            }
+        }
+
+        #[inline]
+        fn initial_layout_requirement(&self) -> ImageLayout {
+            ImageLayout::General
+        }
+
+        #[inline]
+        fn final_layout_requirement(&self) -> ImageLayout {
+            ImageLayout::General
+        }
+
+        #[inline]
+        unsafe fn layout_initialized(&self) {
+            self.layout_initialized.store(true, Ordering::Relaxed);
+        }
+
+        #[inline]
+        fn is_layout_initialized(&self) -> bool {
+            self.layout_initialized.load(Ordering::Relaxed)
+        }
+
+        #[inline]
+        fn descriptor_layouts(&self) -> Option<ImageDescriptorLayouts> {
+            Some(ImageDescriptorLayouts {
+                storage_image: ImageLayout::General,
+                combined_image_sampler: ImageLayout::ShaderReadOnlyOptimal,
+                sampled_image: ImageLayout::ShaderReadOnlyOptimal,
+                input_attachment: ImageLayout::ColorAttachmentOptimal,
+            })
+        }
+    }
+
+    unsafe impl<P> ImageContent<P> for CustomImage {
+        fn matches_format(&self) -> bool {
+            true // FIXME: copied from vulkano::image::StorageImage
+        }
+    }
+
+    impl PartialEq for CustomImage {
+        #[inline]
+        fn eq(&self, other: &Self) -> bool {
+            self.inner() == other.inner()
+        }
+    }
+
+    impl Eq for CustomImage {}
+
+    impl Hash for CustomImage {
+        fn hash<H: Hasher>(&self, state: &mut H) {
+            self.inner().hash(state);
         }
     }
 }
